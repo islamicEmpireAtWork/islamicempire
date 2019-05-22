677a9524c85fdcb9dfed4487f2a03e55d9770eae
pardeewalmsley <alexanderpwalmsley@gmail.com>
2019-05-22 16:32:22 +0200

    New menu

--- src/js/openAbout.js
+++ src/js/openAbout.js
@@ -9,3 +9,29 @@ function closeAbout() {
 document.getElementsByClassName("overlay-content")[0].style.transform = "translateX(0vw)";
 document.getElementById("aboutPane").style.transform = "translate(0vw)";
 }
+
+/* Open Mental Maps div */
+function openMMDiv() {
+  document.getElementsByClassName("admin-maps-menu")[0].style.width = "0vw";
+  document.getElementsByClassName("mental-maps-menu")[0].style.width = "150px";
+}
+
+function closeMMDiv() {
+  document.getElementsByClassName("mental-maps-menu")[0].style.width = "0vw";
+}
+
+/* Open Admin Maps div */
+function openAMDiv() {
+  document.getElementsByClassName("mental-maps-menu")[0].style.width = "0vw";
+  document.getElementsByClassName("admin-maps-menu")[0].style.width = "150px";
+}
+
+function closeAMDiv() {
+  document.getElementsByClassName("admin-maps-menu")[0].style.width = "0vw";
+}
+
+/* Open Admin Maps div */
+function closeMenuDivs() {
+  document.getElementsByClassName("mental-maps-menu")[0].style.width = "0vw";
+  document.getElementsByClassName("admin-maps-menu")[0].style.width = "0vw";
+}
